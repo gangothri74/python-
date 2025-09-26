3e17e585bf023df2b686225fbf4845c53260d1e7
gangothri74 <rachamantiganga@gmail.com>
2025-09-26 12:35:08 +0530

    Add files via upload

---
+++ boolean datatype.py
@@ -0,0 +1,9 @@
+#Write a program which does the following
+'''
+Declare two variables x and y
+Assign the value True to x and the value False to y
+Output x and y space separated on a single line
+'''
+x = True
+y = False
+print(x,y)
