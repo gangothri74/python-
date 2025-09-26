e42df9cdaeb11603dfdaf1fd9273591b25c1d233
gangothri74 <rachamantiganga@gmail.com>
2025-09-26 12:14:09 +0530

    Add files via upload

---
+++ area of rectangle.py
@@ -0,0 +1,8 @@
+#Find out and display the area of a rectangle of sides 45 and 76 respectively.
+#Declare variables length, width and area and assign the relevant values to them
+#Output the value of variable area
+
+length = 76
+width = 45
+area = length*width
+print(area)
