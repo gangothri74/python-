00d8cfd92afb3b5ec92d119ba32872bb7455560c
gangothri74 <rachamantiganga@gmail.com>
2025-09-26 12:42:22 +0530

    Add files via upload

---
+++ area of square.py
@@ -0,0 +1,21 @@
+#Write a program which does the following
+'''
+Find the area of a square of side 14cm.
+Also, find the cost of painting the square if the paint costs is 7$ per 
+c
+m
+2
+cm 
+2
+ .
+Initialize the following variables in your program and use them in the computation
+s to store the length of side the square
+area to store the area of the square
+cost to store the cost of painting
+Output area and cost to the console on separate lines.
+'''
+s = 14
+area = s * s 
+cost = area * 7 
+print(area)
+print(cost)
