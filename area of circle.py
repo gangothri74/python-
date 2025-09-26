04a983e5bf9b7bf854b24755833cac34da290d88
gangothri74 <rachamantiganga@gmail.com>
2025-09-26 12:21:29 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,11 @@
+#Write a program which does the following:
+'''
+Find the area of a circle whose radius is 8.9. Take pi = 3.14
+Declare variables radius , pi and area and assign the relevant values to them
+Output the area, you don't need to output any other text.
+'''
+
+radius = 8.9
+pi = 3.14
+area = pi*radius*radius
+print(area)
