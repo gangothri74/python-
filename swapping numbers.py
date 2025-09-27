f8d21caa26ef18f58101a79fb489ac9294374bd2
gangothri74 <rachamantiganga@gmail.com>
2025-09-27 15:13:32 +0530

    Add files via upload

---
+++ swapping numbers.py
@@ -0,0 +1,14 @@
+#Scenario based questions
+'''
+Can you swap values of a and b using just arithmetic operations and assignment?
+
+Assume a = 3 and b = 5.
+
+Write out your solution in the space given in the IDE.
+'''
+a = 3
+b = 5
+a = a + b
+b = a - b
+a = a - b
+print("After swapping: a-", a, "b-",b)
