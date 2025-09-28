4e784a2df9597c1b29ea18fd27da1d70bcae6f60
gangothri74 <rachamantiganga@gmail.com>
2025-09-28 19:11:07 +0530

    Add files via upload

---
+++ Relational Operators.py
@@ -0,0 +1,10 @@
+'''
+You are given two variables height1 and height2 - use relational operators to output 
+False if they are equal else True
+'''
+
+height1 = 15
+height2 = 13
+# Update the '_' below to solve the problem
+print(height1  != height2)
+
