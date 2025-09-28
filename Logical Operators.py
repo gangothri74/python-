531b66352c2664888ed9464cb12a7efcffee5d9d
gangothri74 <rachamantiganga@gmail.com>
2025-09-28 19:30:29 +0530

    Add files via upload

---
+++ Logical Operators.py
@@ -0,0 +1,9 @@
+#Task
+'''
+You are given a variable height.
+You are allowed to enter the waterpark only if your height is between 5 and 10.
+Apply the condition and output True or False as applicable.
+'''
+height = 15
+# Update the '_' in the code below to solve the problem
+print(height > 5   and   height < 10)
