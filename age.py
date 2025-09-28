bb17d46a327ce100bb69a233df017d3909866f0d
gangothri74 <rachamantiganga@gmail.com>
2025-09-28 19:19:03 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,12 @@
+'''
+You are given the age of a person as the variable age.
+Will this person be able to vote?
+
+Task
+Output True or False assuming that the voting age is 
+18 i.e., a person's age has to be greater than or equal to 18 to vote.
+'''
+age = 20
+vote = 18
+print(vote < age)
+# Update your code below this line
