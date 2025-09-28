112d28e077ae2fe8d753c1a6c8aa694a61ee6388
gangothri74 <rachamantiganga@gmail.com>
2025-09-28 19:04:55 +0530

    Add files via upload

---
+++ sum & product 2 integers.py
@@ -0,0 +1,12 @@
+#Write a program which does the following:
+'''
+Display the sum and product of 2 integers: -50 & 40.
+Initialize the following variables in your program and use them in the computation
+Variable a and assign it the value -50
+Variable b assign it the value 40
+Variables total & product - assign the relevant values to them
+Output total and product as space separated values on a single lines
+'''
+a = -50
+b = 40
+print(a + b, a*b)
