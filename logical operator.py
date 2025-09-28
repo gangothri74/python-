80128bc30d826c53d4e413571f3a726817d64cf4
gangothri74 <rachamantiganga@gmail.com>
2025-09-28 19:39:38 +0530

    Add files via upload

---
+++ logical operator.py
@@ -0,0 +1,12 @@
+'''
+You are given a character in the variable name.
+There is a special rule in Chef town - You will get special treatment if your name is C or A.
+
+Output the following:
+
+True if the name is A or C
+False otherwise
+'''
+name = 'D'
+print(name == 'A' or name == 'C')
+# Update your code below this line
